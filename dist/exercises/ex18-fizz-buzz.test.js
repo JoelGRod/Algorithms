17047c15080e87fa79d4cc6b7b4b18b43dea42ca
joelgrod <joelgrod@gmail.com>
2021-08-04 11:53:57 +0100

    ex 18 + test added

--- dist/exercises/ex18-fizz-buzz.test.js
+++ dist/exercises/ex18-fizz-buzz.test.js
@@ -2,11 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ex18_fizz_buzz_1 = require("./ex18-fizz-buzz");
 describe("Exercise 18", () => {
-    const digit = 10;
+    const fizz = 3;
+    const buzz = 5;
+    const fizzbuzz = 15;
     test("fizzBuzz is defined", () => {
         expect(ex18_fizz_buzz_1.fizzBuzz).toBeDefined();
     });
-    test("fizzBuzz returns correct array", () => {
-        expect(ex18_fizz_buzz_1.fizzBuzz(digit)).toEqual("buzz");
+    test("fizzBuzz returns fizz with multiples of 3", () => {
+        expect(ex18_fizz_buzz_1.fizzBuzz(fizz)).toEqual("fizz");
+    });
+    test("fizzBuzz returns buzz with multiples of 5", () => {
+        expect(ex18_fizz_buzz_1.fizzBuzz(buzz)).toEqual("buzz");
+    });
+    test("fizzBuzz returns fizzbuzz with multiples of 3 AND 5", () => {
+        expect(ex18_fizz_buzz_1.fizzBuzz(fizzbuzz)).toEqual("fizzbuzz");
     });
 });
--- exercises/ex18-fizz-buzz.test.ts
+++ exercises/ex18-fizz-buzz.test.ts
@@ -1,11 +1,19 @@
 import { fizzBuzz } from './ex18-fizz-buzz';
 
 describe("Exercise 18", () => {
-    const digit: number = 10;
+    const fizz: number = 3;
+    const buzz: number = 5;
+    const fizzbuzz: number = 15;
     test("fizzBuzz is defined", () => {
         expect(fizzBuzz).toBeDefined();
     });
-    test("fizzBuzz returns correct array", () => {
-        expect(fizzBuzz(digit)).toEqual("buzz");
+    test("fizzBuzz returns fizz with multiples of 3", () => {
+        expect(fizzBuzz(fizz)).toEqual("fizz");
+    });
+    test("fizzBuzz returns buzz with multiples of 5", () => {
+        expect(fizzBuzz(buzz)).toEqual("buzz");
+    });
+    test("fizzBuzz returns fizzbuzz with multiples of 3 AND 5", () => {
+        expect(fizzBuzz(fizzbuzz)).toEqual("fizzbuzz");
     });
 });
