5e588d848eb4fa02b4928f0029bf8acdaaf12385
joelgrod <joelgrod@gmail.com>
2021-08-04 15:51:11 +0100

    ex 21 + test added

--- dist/exercises/ex21-check-anagram.js
+++ dist/exercises/ex21-check-anagram.js
@@ -1,15 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isAnagram = void 0;
-function convertTextToCharList(text) {
-    return text.toLowerCase().trim().split('').sort();
+function cleanText(text) {
+    return text.toLowerCase().replace(/[^\w]gi/, '').trim().split('').sort().join('');
 }
 function isAnagram(textOne, textTwo) {
-    const textOneList = convertTextToCharList(textOne);
-    const textTwoList = convertTextToCharList(textTwo);
-    return Array.isArray(textOneList) &&
-        Array.isArray(textTwoList) &&
-        textOneList.length === textTwoList.length &&
-        textOneList.every((char, index) => char === textTwoList[index]);
+    return cleanText(textOne) === cleanText(textTwo);
 }
 exports.isAnagram = isAnagram;
+// Example comparing arrays
+// function convertTextToCharList(text: string): string[] {
+//     return text.toLowerCase().replace(/[^\w]gi/, '').trim().split('').sort();
+// }
+// export function isAnagram(textOne: string, textTwo: string): boolean {
+//     const textOneList = convertTextToCharList(textOne);
+//     const textTwoList = convertTextToCharList(textTwo);
+//     return Array.isArray(textOneList) &&
+//            Array.isArray(textTwoList) &&
+//            textOneList.length === textTwoList.length &&
+//            textOneList.every( (char, index) => char === textTwoList[index]);
+// }
--- exercises/ex21-check-anagram.ts
+++ exercises/ex21-check-anagram.ts
@@ -1,16 +1,28 @@
 
-function convertTextToCharList(text: string): string[] {
-    return text.toLowerCase().trim().split('').sort();
+function cleanText(text: string): string {
+    return text.toLowerCase().replace(/[^\w]gi/, '').trim().split('').sort().join('');
 }
 
 export function isAnagram(textOne: string, textTwo: string): boolean {
     
-    const textOneList = convertTextToCharList(textOne);
-    const textTwoList = convertTextToCharList(textTwo);
-    
-    return Array.isArray(textOneList) &&
-           Array.isArray(textTwoList) &&
-           textOneList.length === textTwoList.length &&
-           textOneList.every( (char, index) => char === textTwoList[index]);
+    return cleanText(textOne) === cleanText(textTwo);
 
 }
+
+// Example comparing arrays
+
+// function convertTextToCharList(text: string): string[] {
+//     return text.toLowerCase().replace(/[^\w]gi/, '').trim().split('').sort();
+// }
+
+// export function isAnagram(textOne: string, textTwo: string): boolean {
+    
+//     const textOneList = convertTextToCharList(textOne);
+//     const textTwoList = convertTextToCharList(textTwo);
+    
+//     return Array.isArray(textOneList) &&
+//            Array.isArray(textTwoList) &&
+//            textOneList.length === textTwoList.length &&
+//            textOneList.every( (char, index) => char === textTwoList[index]);
+
+// }
