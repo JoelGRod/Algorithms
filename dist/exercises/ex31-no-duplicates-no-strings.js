806c396df4d90388cdfd5fe3298d99df294496e1
joelgrod <joelgrod@gmail.com>
2021-08-07 14:30:49 +0100

    ex 31 + test added

--- dist/exercises/ex31-no-duplicates-no-strings.js
+++ dist/exercises/ex31-no-duplicates-no-strings.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.removeDuplicatesAndStrings = void 0;
 function removeDuplicatesAndStrings(list) {
-    list = list.filter(element => typeof element !== "string");
+    list = list.filter(element => typeof element === "number");
     return Array.from(new Set(list));
 }
 exports.removeDuplicatesAndStrings = removeDuplicatesAndStrings;
--- exercises/ex31-no-duplicates-no-strings.ts
+++ exercises/ex31-no-duplicates-no-strings.ts
@@ -2,7 +2,7 @@
 
 export function removeDuplicatesAndStrings(list: any[]) {
 
-    list = list.filter( element => typeof element !== "string");
+    list = list.filter( element => typeof element === "number");
 
     return Array.from(new Set(list));
 }
