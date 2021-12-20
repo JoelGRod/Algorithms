2fce51fc2d5fcd3ede365ec58e7c7baf878fec2d
joelgrod <joelgrod@gmail.com>
2021-12-20 19:19:09 +0000

    ex20

---
+++ exercisesJs/ex20-letterIsPangram.js
@@ -0,0 +1,31 @@
+/**
+ * En la clase de español del pueblo de Laponia han creado un
+ * reto a la hora de escribir la carta a Papa Noél 🎅:
+ * la carta ✉️ tiene que contener todas las letras del alfabeto.
+ *
+ * Desde el taller de Santa 🎅 se han enterado y quieren escribir
+ * una función que les diga si realmente la cadena de texto que
+ * les llega tiene, efectivamente, todas las letras del abecedario
+ * español 🔎.
+ *
+ * Hay que tener en cuenta las letras en mayúscula y que las
+ * letras con acento y diéresis se consideran iguales.
+ * Por ejemplo la á y la ä cuenta como una a.
+ *
+ * Vamos a ver unos ejemplos de frases:
+ * pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho') // true
+ * pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!') // true
+ *
+ * pangram('Esto es una frase larga pero no tiene todas las letras del abecedario') // false
+ * pangram('De la a a la z, nos faltan letras') // false
+ *
+ * Y ya que estás... ¿Cuál es tu pangrama favorito?
+ * ¡Compártelo en nuestra comunidad de Discord!
+ */
+
+export default function pangram(letter) {
+  return new Set(letter
+    // .normalize("NFD")
+    // .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-zñáéíóúü]/gi, "")).size > 26;
+}
