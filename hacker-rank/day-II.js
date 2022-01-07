6dc3632bda0dd14b727dd8a57c422b53fd402711
JoelGRod <joelgrod@gmail.com>
2022-01-07 01:28:20 +0000

    lonely int

--- hacker-rank/day-II.js
+++ hacker-rank/day-II.js
@@ -1,4 +1,26 @@
 /**
+ * Given an array of integers, where all elements but one 
+ * occur twice, find the unique element.
+ * 
+ * Example
+ * arr = [1,2,3,4,3,2,1]
+ * The unique element is 4
+ */
+
+ function lonelyinteger(a) {
+    // Write your code here
+    const classifier = {};
+    a.forEach( value => {
+        value in classifier 
+        ? classifier[value]++
+        : classifier[value] = 1
+    });
+    
+    for(let value in classifier) 
+        if(classifier[value] === 1) return value;
+}
+
+/**
  * Given a square matrix, calculate the absolute difference
  * between the sums of its diagonals.
  *
