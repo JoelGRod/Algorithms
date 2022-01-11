e5d8017e67b7781ca0df5baca768b3b40e6f5019
JoelGRod <joelgrod@gmail.com>
2022-01-11 12:19:51 +0000

    balanced brackets

--- hacker-rank/day-V.js
+++ hacker-rank/day-V.js
@@ -24,7 +24,7 @@ function mergeLists(head1, head2) {
 }
 
 /**
- * Queue using Two Stacks
+ * Queue using Two Stacks - FIFO - First In First Out
  * A queue is an abstract data type that maintains
  * the order in which elements were added to it,
  * allowing the oldest elements to be removed from
@@ -117,4 +117,49 @@ function processDataExtraTwo(input) {
     }
     list.push(value);
   });
+}
+
+/**
+ * Balanced Brackets - FILO - First In Last Out
+ * A bracket is considered to be any one of the following
+ * characters: (, ), {, }, [, or ].
+ *
+ * Two brackets are considered to be a matched pair if the
+ * an opening bracket (i.e., (, [, or {) occurs to the left of
+ * a closing bracket (i.e., ), ], or }) of the exact same type.
+ * There are three types of matched pairs of brackets: [], {}, and ().
+ *
+ * A matching pair of brackets is not balanced if the set of
+ * brackets it encloses are not matched. For example, {[(])}
+ * is not balanced because the contents in between { and }
+ * are not balanced. The pair of square brackets encloses a
+ * single, unbalanced opening bracket, (, and the pair of
+ * parentheses encloses a single, unbalanced closing square bracket, ].
+ *
+ * By this logic, we say a sequence of brackets is balanced if
+ * the following conditions are met:
+ * - It contains no unmatched brackets.
+ * - The subset of brackets enclosed within the confines of a matched pair
+ * of brackets is also a matched pair of brackets.
+ *
+ * Given n  strings of brackets, determine whether each sequence of
+ * brackets is balanced. If a string is balanced, return YES.
+ * Otherwise, return NO.
+ */
+
+function isBalanced(s) {
+  // Write your code here
+  const brackets = "{}[]()<>";
+  const filo = [];
+
+  for (let bracket of s) {
+    let bracketIdx = brackets.indexOf(bracket);
+    if (bracketIdx === -1) continue;
+    if (bracketIdx % 2 === 0) {
+      filo.unshift(bracketIdx + 1);
+    } else {
+      if (filo.shift() !== bracketIdx) return "NO";
+    }
+  }
+  return "YES";
 }
